51ffe21fc435568cca10d6228ee0eef21f409b96
robsonpaulo81-ux <robson.paulo.81@gmail.com>
2026-08-30 20:06:10 -0300

    Add function to send SMS using Twilio API

---
+++ src/lib/twilio-sms.ts
@@ -0,0 +1,23 @@
+export async function enviarSMS(telefone: string, mensagem: string) {
+  const sid = process.env.TWILIO_ACCOUNT_SID;
+  const token = process.env.TWILIO_AUTH_TOKEN;
+  const from = process.env.TWILIO_SMS_FROM; // número SMS da Twilio
+
+  const res = await fetch(
+    `https://api.twilio.com/2010-04-01/Accounts/${sid}/Messages.json`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: "Basic " + Buffer.from(`${sid}:${token}`).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        To: telefone,
+        From: from!,
+        Body: mensagem,
+      }),
+    }
+  );
+
+  return res.json();
+}
